af582153197b57662cb09a03c3b8c2e3ba9a1a52
Benjamin-Mangandi <3011042200101@ingenieria.usac.edu.gt>
2023-09-05 20:18:02 -0600

    Interfazv0.3

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import graphviz
 import tkinter
+from tkinter import ttk
+
+
+def analizar():
+    print("analizar")
+
+
+def buscando_errores():
+    print("Error")
+
+
+def reporte_documento():
+    print("Reporte")
+
+
 menu = tkinter.Tk()
-menu.geometry("800x600")
-menu.config(bg="green")
-analizar_boton = tkinter.Button(menu, text="Analizar Texto")
-analizar_boton.grid(column=0, row=0, padx=40, pady=5)
-errores_boton = tkinter.Button(menu, text="Ver Errores")
-errores_boton.grid(column=1, row=0, padx=40, pady=5)
+menu.geometry("800x600", )
+menu.config(bg="#08227E")
+menu.title("Reportes")
+opciones = ("abrir", "Guardar", "Guardar Como", "Salir")
+frame_botones = tkinter.Frame(menu, background="#0E74CA")
+frame_botones.pack(side="top", fill="x")
+opciones_boton = ttk.Combobox(
+    frame_botones, background="#0ECA19", text="Archivo", values=opciones)
+analizar_boton = tkinter.Button(
+    frame_botones, background="#0ECA19", text="Analizar Texto", command=analizar)
+errores_boton = tkinter.Button(
+    frame_botones, background="#0ECA19", text="Ver Errores", command=buscando_errores)
+reporte_boton = tkinter.Button(
+    frame_botones, background="#0ECA19", text="Reporte", command=reporte_documento)
+cuadro_de_texto = tkinter.Text(menu, wrap="word")
+opciones_boton.pack(side="left", padx=50, pady=10)
+reporte_boton.pack(side="left", padx=50, pady=10)
+analizar_boton.pack(side="left", padx=50, pady=10)
+errores_boton.pack(side="left", padx=50, pady=10)
+cuadro_de_texto.pack(side="left", padx=70)
 menu.mainloop()
