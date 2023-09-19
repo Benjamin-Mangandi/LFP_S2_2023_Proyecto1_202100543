3c4e06f3ff25a02e9a103e9b165216960a1883b6
Benjamin-Mangandi <3011042200101@ingenieria.usac.edu.gt>
2023-09-18 22:04:43 -0600

    analizadorv0.4

---
+++ objetos/errores.py
@@ -0,0 +1,7 @@
+class formato_error:
+    def __init__(self, numero, lexema, tipo, columna, fila):
+        self.numero = numero
+        self.lexema = lexema
+        self.tipo = tipo
+        self.columna = columna
+        self.fila = fila
--- operaciones/analizador_lexico.py
+++ operaciones/analizador_lexico.py
@@ -1,35 +1,49 @@
 from objetos import aritmetica
 from objetos import estilo
+from objetos import errores
 import json
-def analizar_palabra(abecedario, palabra):
+def analizar_palabra(abecedario, palabra,numero,columna, fila):
     for letra in palabra:
         if letra not in abecedario:
+            nuevo_error = errores.formato_error(numero,letra,"Error Lexico", columna, fila)
+            errores_validados.append(nuevo_error)
             print(letra)
-
+errores_validados = []
 datos_validados = []
 datos_estilo = []
 inicio2 = ["valor1", "valor2"]
 inicio = ["operaciones", "configuraciones", "operacion"]
-abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-              "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+abecedario = ["{","}","[","]", ":", ",", '"' , " ", "\n" ,"=" ,"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+              "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3","4", "5", "6", "7", "8", "9"]
 operaciones_aritmeticas = ["suma", "resta",
                            "multiplicacion", "division", "potencia"]
 
-
 def analizar(texto):
-    datos = json.loads(texto)
+    nuevo_texto=texto
+    columna =1
+    fila = 0
+    for letra in texto:
+        fila=fila+1
+        if letra =="\n":
+            columna=columna+1
+            fila=0
+        if letra.lower() not in abecedario:
+            nuevo_texto = nuevo_texto.replace(letra, "")
+            print(letra, "columna: "+str(columna)+"fila: "+str(fila))
+    print(nuevo_texto)
+    datos = json.loads(nuevo_texto)
     with open("archivo.json", "w") as archivo:
         json.dump(datos, archivo, indent=4)
     with open("archivo.json", "r") as archivo:
         datos_ingresados = json.load(archivo)
     i=0
+    num_error=0
     for configuracion in datos_ingresados:
         if configuracion.lower() in inicio:
             if configuracion.lower() == "operaciones":
                 for tipo in datos_ingresados["operaciones"]:
                     nombres = 0
                     nombres = tipo.keys()
-                    print(i)
                     for nombre in nombres:
                         if nombre in inicio2:
                             if nombre == "valor2":
@@ -41,14 +55,10 @@ def analizar(texto):
                                 i=i+1
                                 break
                         elif nombre not in inicio:
-                            analizar_palabra(abecedario, nombre)
+                            num_error=num_error+1
+                            analizar_palabra(abecedario, nombre, num_error,i,i)
                             i=i+1
-                            break
-                        
-
-
-                        
-    
+                            break         
             elif configuracion.lower() == "configuraciones":
                 for i in range(len(datos_ingresados["configuraciones"])):
                     nuevo_texto = datos_ingresados[configuracion][i]["texto"]
@@ -58,6 +68,6 @@ def analizar(texto):
                     nuevo_estilo = estilo.estilo_grafico(nuevo_texto,nuevo_fondo, nueva_fuente, nueva_forma)
                     datos_estilo.append(nuevo_estilo)
         else:
-            analizar_palabra(abecedario,configuracion.lower())
+            num_error=num_error+1
+            analizar_palabra(abecedario,configuracion.lower(),num_error,i,i)
             break
-    print(datos_validados)
