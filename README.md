72f238d424d7e7d816c67737d325d7ea0f5277b3
Benjamin-Mangandi <3011042200101@ingenieria.usac.edu.gt>
2023-09-23 18:58:49 -0600

    Analizadorv1.01

--- README.md
+++ README.md
@@ -11,24 +11,38 @@ Bibliotecas usadas: graphviz, json, tkinter, subprocess,graphviz, math
 ```
 ---
 ### Descripción del Proyecto
-
+El siguiente proyecto es un analizador lexico, el cual tiene como objetivo reconocer errores lexico ingresados por el usuario haciendo uso del formato .json, mostrarle al usuario los posibles errores al momento de leer un archivo y graficar las operaciones ingresadas por medio de nodos utilizando para eso la libreria graphiz.
 ### Partes del Proyecto
-
-
 ## **Archivos y Carpetas**
 ## Carpeta Principal
 # main.py
-Es en donde se encuentra el iniciador de la ventana principal, contiene todos los elementos de tkinter
+**Importa**: *analizador_lexico, graficador*
+Es en donde se encuentra el iniciador de la ventana principal: *menu*, contiene todos los elementos de tkinter de la ventana principal, la cual cuenta con un cuadro de texto por medio de *tkinter.text* el cual sirve para que el usuario ingrese las operaciones deseadas o bien cargue un archivo mediante el boton *abrir* el cual está como comando en un *tkinter.Menu*; se puede sobreescribir el archivo, guardarlo como otro diferente o salir de la aplicacion
+![main](https://i.ibb.co/85dsk2Q/menu.png)
 # **Funciones**
 ### *seleccionar_archivo*
+**Parametros**: nombre del cuadro de texto
+Esta función permite abrir un archivo de texto al usuario por medio de la función de tkinter 
+*filedialog.askopenfilename()* inserta el texto en el cuadro de texto y reinicia los nodos anteriores si es que tuvieran datos de analisis anteriores por medio de la función *graficador.reiniciar()* y asigna la ruta a la variable *ruta_archivo*
+![archivo](https://i.ibb.co/tBpNm9y/seleccionar-archivo.png)
 ### *guardar*
+**Parametros**: nombre del cuadro de texto
+Esta función permite o bien sobreescribir el archivo si es que existe o crear uno nuevo y guardarlo con el nombre que desee el usuario por medio de una validación de la variable *ruta_archivo*, si es que contiene la ruta de un archivo abierto.
+![guardar](https://i.ibb.co/Qksqjkq/guardar.png)
 ### *guardar_como*
+**Parametros**: nombre del cuadro de texto
+Esta función permite guardar como un archivo aparte el texto que el usuario haya ingresado y le asigna la ruta a la variable *ruta_archivo*
 ### *salir*
 Esta funcion simplemente cierra la ventana junto con el proceso con la funcion *destroy()*
+![guardar_salir](https://i.ibb.co/fnnpsrD/guardar-como-y-salir.png)
 ### *buscando_errores*
+![buscando_errores](https://i.ibb.co/NtS74XV/buscando-errores.png)
 ### *reporte_documento*
-# Manual_usuario.md
-# README.md
+Esta funcion determina si hay operaciones ingresadas por el usuario por medio de una condicion con la varible del archivo **analizador_lexico** *datos_validados* si los hay grafica normalmente, si no, manda una advertencia al usuario.
+![reporte](https://i.ibb.co/RQwqTQ8/reporte-documento.png)
+### *manual_usuario y manual_tecnico*
+Estas funciones simplemente abren en el bloc de notas los archivos del manual de usuario y tecnico respectivamente
+![usuario_tecnico](https://i.ibb.co/M71RbzX/usuario-tecnico.png)
 ## Operaciones
 # analizador.py
 # **Funciones**
@@ -51,7 +65,7 @@ Esta funcion simplemente cierra la ventana junto con el proceso con la funcion *
 # **Funciones**
 # estilo.py
 # **Funciones**
-![Codigo_menu](https://i.ibb.co/85dsk2Q/menu.png)
+
 
 # **Funciones**
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter
 from tkinter import *
-from tkinter import ttk
 from tkinter import filedialog
 from operaciones import graficador
 from operaciones import analizador_lexico
@@ -19,10 +18,13 @@ def seleccionar_archivo(cuadro_texto):
         graficador.reiniciar()
     except Exception:
         return
+
+
 def guardar(cuadro_texto):
     if ruta_archivo.get() == "":
         texto = cuadro_texto.get("1.0", END)
-        archivo_guardado = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Archivos de texto", "*.txt")])
+        archivo_guardado = filedialog.asksaveasfilename(
+            defaultextension=".txt", filetypes=[("Archivos de texto", "*.txt")])
         if archivo_guardado:
             with open(archivo_guardado, "w") as archivo:
                 archivo.write(texto)
@@ -32,16 +34,23 @@ def guardar(cuadro_texto):
             with open(ruta_archivo.get(), "w") as archivo:
                 archivo.write(texto)
         except Exception:
-            print("no se pudo guardar el archivo")
-def guardar_como (cuadro_texto):
+            print("No se pudo guardar el archivo")
+
+
+def guardar_como(cuadro_texto):
     texto = cuadro_texto.get("1.0", END)
-    archivo_guardado = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Archivos de texto", "*.txt")])
+    archivo_guardado = filedialog.asksaveasfilename(
+        defaultextension=".txt", filetypes=[("Archivos de texto", "*.txt")])
     if archivo_guardado:
         with open(archivo_guardado, "w") as archivo:
             archivo.write(texto)
     ruta_archivo.set(archivo)
+
+
 def salir():
     menu.destroy()
+
+
 def manual_usuario():
     try:
         archivo = 'LFP_S2_2023_Proyecto1_202100543/Manual_usuario.md'
@@ -50,6 +59,8 @@ def manual_usuario():
         return
     except Exception:
         return
+
+
 def manual_tecnico():
     try:
         archivo = 'LFP_S2_2023_Proyecto1_202100543/README.md'
@@ -59,26 +70,44 @@ def manual_tecnico():
     except Exception:
         return
 
+
 def buscando_errores(errores):
     if len(errores["Errores"]) < 1:
         notificacion = tkinter.Toplevel()
         notificacion.title("Alerta")
         notificacion.geometry("400x150")
-        etiqueta = tkinter.Label(notificacion, text="No se han encontrado errores o aún no ha analizado el archivo.")
+        etiqueta = tkinter.Label(
+            notificacion, text="No se han encontrado errores o aún no ha analizado el archivo.")
         etiqueta.pack(padx=20, pady=20)
-        boton_cerrar = tkinter.Button(notificacion, background="#FF1919",text="Cerrar", command=notificacion.destroy)
+        boton_cerrar = tkinter.Button(
+            notificacion, background="#FF1919", text="Cerrar", command=notificacion.destroy)
         boton_cerrar.pack(pady=10)
-        boton_cerrar.config(height=2,width=10)
+        boton_cerrar.config(height=2, width=10)
     else:
         try:
             with open("RESULTADOS_202100543.json", "w", encoding='utf-8') as archivo:
-                json.dump(analizador_lexico.errores_validados, archivo, indent=4, ensure_ascii=False)
+                json.dump(analizador_lexico.errores_validados,
+                          archivo, indent=4, ensure_ascii=False)
             subprocess.Popen(['notepad.exe', "RESULTADOS_202100543.json"])
         except Exception:
             return
 
+
 def reporte_documento():
-    graficador.graficar(analizador_lexico.datos_estilo)
+    if len(analizador_lexico.datos_validados)!=0:
+        graficador.graficar(analizador_lexico.datos_estilo)
+    else:
+        notificacion = tkinter.Toplevel()
+        notificacion.title("Alerta")
+        notificacion.geometry("400x150")
+        etiqueta = tkinter.Label(
+            notificacion, text="No se han encontrado datos para graficar")
+        etiqueta.pack(padx=20, pady=20)
+        boton_cerrar = tkinter.Button(
+            notificacion, background="#FF1919", text="Cerrar", command=notificacion.destroy)
+        boton_cerrar.pack(pady=10)
+        boton_cerrar.config(height=2, width=10)
+
 
 menu = tkinter.Tk()
 menu.geometry("800x600", )
@@ -88,26 +117,31 @@ frame_botones = tkinter.Frame(menu, background="#3D3D3F")
 frame_botones.pack(side="top", fill="x")
 barra_de_opciones = tkinter.Menu(menu)
 opciones_ayuda = tkinter.Menu(barra_de_opciones, tearoff=0)
-opciones_ayuda.add_command(label="Manual Tecnico", command=lambda: manual_tecnico())
-opciones_ayuda.add_command(label="Manual de Usuario", command=lambda: manual_usuario())
+opciones_ayuda.add_command(label="Manual Tecnico",
+                           command=lambda: manual_tecnico())
+opciones_ayuda.add_command(label="Manual de Usuario",
+                           command=lambda: manual_usuario())
 opciones_archivo = tkinter.Menu(barra_de_opciones, tearoff=0)
-opciones_archivo.add_command(label="Abrir", command=lambda: seleccionar_archivo(cuadro_de_texto))
-opciones_archivo.add_command(label="Guardar", command=lambda: guardar(cuadro_de_texto))
-opciones_archivo.add_command(label="Guardar Como", command=lambda: guardar_como(cuadro_de_texto))
+opciones_archivo.add_command(
+    label="Abrir", command=lambda: seleccionar_archivo(cuadro_de_texto))
+opciones_archivo.add_command(
+    label="Guardar", command=lambda: guardar(cuadro_de_texto))
+opciones_archivo.add_command(
+    label="Guardar Como", command=lambda: guardar_como(cuadro_de_texto))
 opciones_archivo.add_command(label="Salir", command=salir)
 analizar_boton = tkinter.Button(
-    frame_botones, background="#0ECA19", height=2,width=13 ,text="Analizar Texto", command=lambda: analizador_lexico.analizar(cuadro_de_texto.get("1.0", END)))
+    frame_botones, background="#0ECA19", height=2, width=13, text="Analizar Texto", command=lambda: analizador_lexico.analizar(cuadro_de_texto.get("1.0", END)))
 errores_boton = tkinter.Button(
-    frame_botones, background="#0ECA19",height=2,width=13, text="Ver Errores", command=lambda: buscando_errores(analizador_lexico.errores_validados))
+    frame_botones, background="#0ECA19", height=2, width=13, text="Ver Errores", command=lambda: buscando_errores(analizador_lexico.errores_validados))
 reporte_boton = tkinter.Button(
-    frame_botones, background="#0ECA19",height=2,width=13, text="Reporte", command=reporte_documento)
+    frame_botones, background="#0ECA19", height=2, width=13, text="Reporte", command=reporte_documento)
 cuadro_de_texto = tkinter.Text(menu)
 analizar_boton.pack(side="left", padx=80, pady=15)
 errores_boton.pack(side="left", padx=80, pady=15)
 reporte_boton.pack(side="left", padx=80, pady=15)
 cuadro_de_texto.pack(side="left", padx=70)
 barra_de_opciones.add_cascade(label="Archivo", menu=opciones_archivo)
-barra_de_opciones.add_cascade(label="Ayuda",menu=opciones_ayuda)
+barra_de_opciones.add_cascade(label="Ayuda", menu=opciones_ayuda)
 ruta_archivo = tkinter.StringVar()
 menu.config(menu=barra_de_opciones)
 menu.resizable(False, False)
